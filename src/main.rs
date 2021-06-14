9ce560c0f25b109a671f53b7945229eff611fa16
wangweigit3077 <18779223077@163.com>
2021-06-14 11:15:49 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+use futures::{Future};
+use hyper::{Client, Uri};
+use tokio_core::reactor::Core;
 
 fn main() {
-    println!("hello ws rold123");
+    // Core is the Tokio event loop used for making a non-blocking request
+    let mut core = Core::new().unwrap();
+
+    let client = Client::new();
+
+    let url : Uri = "http://pv.sohu.com/cityjson?ie=utf-8".parse().unwrap();
+    // assert_eq!(url.query(), Some("foo=bar"));
+
+    let request_result = core.run(client
+        .get(url)
+        .map(|res| {
+            println!("Response: {:?}", res.body());
+        })
+        .map_err(|err| {
+            println!("Error: {}", err);
+       })
+    );
+
 }
